245163d87681a161212216c7730f3c5cc5ac0e70
Simon Date <simondate@yahoo.co.uk>
2018-07-08 14:26:21 +0100

    58

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,16 @@
+import axios from 'axios';
+
 const API_KEY = 'ac2178ed8090891137a0382d69e70739';
+const ROOT_URL = `https://openweathermap.org/api/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+  const url = `${ROOT_URL}&q=${city},gb`;
+  const request = axios.get(url);
+
+  return {
+    type: FETCH_WEATHER,
+    payload: request
+  };
+}
